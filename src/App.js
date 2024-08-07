578382e7cc179e9b27f6db14ca692da8b5b5615e
Suyash <103137186+MasterSuyash1@users.noreply.github.com>
2024-08-07 13:33:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-<<<<<<< HEAD
-import Dashboard from './components/Dashboard'; // Import the Dashboard component
-=======
 import Dashboard from './components/Dashboard';
 import ProgrammingAssignment from './components/ProgrammingAssignment';
 import AdminDashboard from './components/AdminDashboard'; // Import the AdminDashboard component
->>>>>>> main
 
 function App() {
   return (
@@ -20,13 +16,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-          <Route path="/dashboard" element={<Dashboard />} /> {/* Add the Dashboard route */}
-=======
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/programming-assignment" element={<ProgrammingAssignment />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} /> {/* Add the AdminDashboard route */}
->>>>>>> main
         </Routes>
       </Router>
     </ChakraProvider>
@@ -34,7 +26,3 @@ function App() {
 }
 
 export default App;
-<<<<<<< HEAD
-
-=======
->>>>>>> main
